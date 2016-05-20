ce87594b39da9a1672bd13e927ccb31b58f658db
Huidong Tian <tienhuitung@gmail.com>
2016-05-20 12:25:25 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -9,17 +9,19 @@
    </head>
    <body onload="setTimeout('init(0,25,1);', 100);">
 
-      
-      <div class="container-fluid text-center liveimage">
-         <img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=time()&amp;pDelay=40000">
+         <img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=time()&amp;pDelay=10000">
+         
          <div id="main-buttons" >
             <input id="video_button" type="button" class="btn btn-primary">
             <input id="image_button" type="button" class="btn btn-primary">
             <input id="timelapse_button" type="button" class="btn btn-primary">
             <input id="md_button" type="button" class="btn btn-primary">
             <input id="halt_button" type="button" class="btn btn-danger">
+            <a href="preview.php" class="btn btn-default">Download Videos and Images</a>
+            <input id="shutdown_button" type="button" value="shutdown system" onclick="sys_shutdown();" class="btn btn-danger">
+            <input id="reboot_button" type="button" value="reboot system" onclick="sys_reboot();" class="btn btn-danger">
          </div>
-      </div>
-      <a href="preview.php" class="btn btn-default">Download Videos and Images</a>
+ 
+      
    </body>
 </html>
